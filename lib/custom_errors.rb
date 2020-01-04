3710e43b3cef34fa953b8ee2efb20b7a7cff4a60
Joseph Ruiz <ruizj3@uw.edu>
2020-01-04 20:33:35 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,13 +9,11 @@ class Person
     self.partner = person
     person.partner = self
   end
-
+  # Add the following two lines:
+  class PartnerError < StandardError
+  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
